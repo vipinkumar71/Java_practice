163bbbab44786dc65af8a7eae0e934e281b35e72
Vipin Kumar <vipinthakur3881@gmail.com>
2022-03-14 00:23:16 +0530

    Some String method explanation

--- src/String_Method.java
+++ src/String_Method.java
@@ -2,9 +2,40 @@ import java.util.Scanner;
 
 public class String_Method {
     public static void main(String[] args){
+        //String isEmpty method and this method return us boolean i.e true and false
        String s = "Hello";
        String another = "";
        boolean a= another.isEmpty();
         System.out.println(a);
+
+// String equals method this method do comparision of two String
+
+//        String s= "Hello";
+//        String another= "Hello";
+//
+//        if (s.equals(another)){
+//            System.out.println("yes, those string are equals");
+//        }else{
+//            System.out.println("They are not equals");
+//        }
+
+        //String trim method this method  trim the empty spaces
+//        String s="    Hello    ";
+//        String another = "vipin";
+//        String a=s.trim();
+//        System.out.println(a);
+
+        //String length method this method return us int and count the how many alphabet are there
+//        String s="Hello";
+//       int a= s.length();
+//        System.out.println(a);
+
+        //String toUppercase method this method convert all the alphabet into uppercase
+         String name = "vipin";
+         String myName = name.toUpperCase();
+        System.out.println(myName);
+
+
+
     }
 }
