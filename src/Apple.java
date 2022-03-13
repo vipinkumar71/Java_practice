e83e02c5e9bd682270547cea5d73e30e0f4a059b
Vipin Kumar <vipinthakur3881@gmail.com>
2022-03-13 23:29:50 +0530

    rename the file and set the name of the file Main

--- src/Apple.java
+++
@@ -1,6 +0,0 @@
-public class Apple {
-    public static void main(String[] args) {
-      int c =  Return_Statement.plusAgain();
-        System.out.println(c);
-    }
-}
---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+//      int c =  Return_Statement.plus10();
+//        System.out.println(c);
+
+        System.out.println(Return_Statement.plus10());
+    }
+}
